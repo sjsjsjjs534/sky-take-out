5effb2533d8d8d9e7fcd98f20b27d753147290f6
zhangyuruo <1229944289@qq.com>
2024-10-16 20:25:25 +0800

    文件上传代码书写

--- sky-take-out/sky-server/src/main/java/com/sky/controller/admin/CommonController.java
+++ sky-take-out/sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -1,5 +1,6 @@
 package com.sky.controller.admin;
 
+import com.sky.constant.MessageConstant;
 import com.sky.result.Result;
 import com.sky.utils.AliOssUtil;
 import io.swagger.annotations.Api;
@@ -44,6 +45,6 @@ public class CommonController {
         } catch (IOException e) {
             log.info("文件上传失败：{}",e);
         }
-        return null;
+        return Result.error(MessageConstant.UPLOAD_FAILED);
     }
 }
