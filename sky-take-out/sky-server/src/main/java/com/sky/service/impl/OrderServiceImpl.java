220cc5a51152a012d4c47b2e017c0ce3de16913c
zhangyuruo <1229944289@qq.com>
2024-11-10 11:26:37 +0800

    来单提醒代码开发

--- sky-take-out/sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
+++ sky-take-out/sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
@@ -21,6 +21,7 @@ import com.sky.vo.OrderPaymentVO;
 import com.sky.vo.OrderStatisticsVO;
 import com.sky.vo.OrderSubmitVO;
 import com.sky.vo.OrderVO;
+import com.sky.websocket.WebSocketServer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -61,6 +62,8 @@ public class OrderServiceImpl implements OrderService {
     private UserMapper userMapper;
     @Autowired
     private WeChatPayUtil weChatPayUtil;
+    @Autowired
+    private WebSocketServer webSocketServer;
 
     @Value("${sky.shop.address}")
     private String shopAddress;
@@ -186,12 +189,12 @@ public class OrderServiceImpl implements OrderService {
 
         //通过websocket向客户端浏览器推送消息type orderId content
         Map map = new HashMap();
-        map.put("type",1);//1.来电提醒 2.客户催单
+        map.put("type",1);//1.来单提醒 2.客户催单
         map.put("orderId", orders.getId());
         map.put("content", "订单号："+outTradeNo);
 
         String json = JSON.toJSONString(map);
-//        webSocketServer.sendToAllClient(json);
+        webSocketServer.sendToAllClient(json);
     }
 
     /*
